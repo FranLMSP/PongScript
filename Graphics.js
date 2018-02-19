dd4a54e042129c1ccb594f50da8c2b8b947a5cf8
Franco <francolmenagar2@hotmail.com>
2018-02-19 17:27:58 -0400

    Drawing a number nine

--- Graphics.js
+++ Graphics.js
@@ -348,6 +348,39 @@ class Graphics
 				this.drawRect(x+40 , y+60, 10, 10,'#bdbdbd');
 				break;
 			case 9:
+				this.drawRect(x+10 , y, 10, 10,'#bdbdbd');
+				this.drawRect(x+20 , y, 10, 10,'#bdbdbd');
+				this.drawRect(x+30 , y, 10, 10,'#bdbdbd');
+				this.drawRect(x+40 , y, 10, 10,'#bdbdbd');
+
+				this.drawRect(x    , y+10, 10, 10,'#bdbdbd');
+				this.drawRect(x+10 , y+10, 10, 10,'#bdbdbd');
+				this.drawRect(x+40 , y+10, 10, 10,'#bdbdbd');
+				this.drawRect(x+50 , y+10, 10, 10,'#bdbdbd');
+
+				this.drawRect(x    , y+20, 10, 10,'#bdbdbd');
+				this.drawRect(x+10 , y+20, 10, 10,'#bdbdbd');
+				this.drawRect(x+40 , y+20, 10, 10,'#bdbdbd');
+				this.drawRect(x+50 , y+20, 10, 10,'#bdbdbd');
+
+				this.drawRect(x+10 , y+30, 10, 10,'#bdbdbd');
+				this.drawRect(x+20 , y+30, 10, 10,'#bdbdbd');
+				this.drawRect(x+30 , y+30, 10, 10,'#bdbdbd');
+				this.drawRect(x+40 , y+30, 10, 10,'#bdbdbd');
+				this.drawRect(x+50 , y+30, 10, 10,'#bdbdbd');
+
+				this.drawRect(x+40 , y+40, 10, 10,'#bdbdbd');
+				this.drawRect(x+50 , y+40, 10, 10,'#bdbdbd');
+
+				this.drawRect(x    , y+50, 10, 10,'#bdbdbd');
+				this.drawRect(x+10 , y+50, 10, 10,'#bdbdbd');
+				this.drawRect(x+40 , y+50, 10, 10,'#bdbdbd');
+				this.drawRect(x+50 , y+50, 10, 10,'#bdbdbd');
+
+				this.drawRect(x+10 , y+60, 10, 10,'#bdbdbd');
+				this.drawRect(x+20 , y+60, 10, 10,'#bdbdbd');
+				this.drawRect(x+30 , y+60, 10, 10,'#bdbdbd');
+				this.drawRect(x+40 , y+60, 10, 10,'#bdbdbd');
 				break;
 			case 10:
 				break;
