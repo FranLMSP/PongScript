1d3dd2d1e8ab66e2b3c7e3521a980f669c40728e
Franco <francolmenagar2@hotmail.com>
2018-02-19 15:36:56 -0400

    Changing some physics

--- Ball.js
+++ Ball.js
@@ -1,12 +1,12 @@
 class Ball
 {
-	constructor(r = 40,x=(640/2),y=(480/2), min_speed = 0.3, max_speed = 10, friction = 0.3, aceleration = 1)
+	constructor(r = 40,x=(640/2),y=(480/2), min_speed = 1, max_speed = 10, friction = 0.3, aceleration = 1)
 	{
 		this.r = r;
 		this.x = x;
 		this.y = y;
 
-		this.vspeed = 0.1;
+		this.vspeed = 0;
 		this.hspeed = min_speed;
 
 		this.aceleration = aceleration;
@@ -17,7 +17,7 @@ class Ball
 		// this.x++;
 		// console.log(this.x);
 
-		for(let angle = 0; angle <= 360; angle+=10)
+		for(let angle = 0; angle <= 360; angle+=0.5)
 		{
 			let x = (this.r * Math.cos(angle)) + this.x;
 			let y = (this.r * Math.sin(angle)) + this.y;
@@ -26,36 +26,61 @@ class Ball
 				(y>= player.y && y <= (player.y+player.h) ) // vertical collision
 			 )
 			{
-				if(angle>=0 && angle <=90)
-				{
-					this.hspeed = -this.aceleration;
+				let diagonalCol = false;
+				//RIGTH COLLISION
+				if(angle>=157 && angle <=202)
+					this.hspeed=-this.hspeed;
 
-					// this.x-= (x-player.x);
-					// this.y-= (y-player.y);
-				}
-				else if(angle >=90 && angle <=180)
-				{
-					this.hspeed = this.aceleration;
-					
-					// this.x+= (x -(player.x+player.w));
-					// this.y-= (player.y - y);
-				}
-				else if(angle >=180 && angle <=270)
-				{
-					this.hspeed = this.aceleration;
-					
-					// this.x+= (x -(player.x+player.w));
-					// this.y-= (y -(player.y+player.h));
-				}
-				else if(angle >=270 && angle <=360)
-				{					
-					this.hspeed = -this.aceleration;
+				//LEFT COLLISION
+				if((angle>=337 && angle<=360) || (angle>=0 && angle<=225))
+					this.hspeed=-this.hspeed;
 
-					// this.x+= (x-player.x);
-					// this.y-= (x -(player.x+player.w));
+				//UP COLLISION
+				if(angle>=247 && angle<=292)
+					this.vspeed=-this.vspeed;
+
+				//DOWN COLLISION
+				if(angle<=112 && angle>=67)
+					this.vspeed=-this.vspeed;
+
+
+				//DIAGONAL COLLISIONS
+
+				//UP LEFT COLLISION
+				if(angle>=202 && angle<=247)
+				{
+					this.vspeed = (this.vspeed<0)?-this.vspeed:this.vspeed;
+					this.hspeed = (this.hspeed<0)?-this.hspeed:this.hspeed;
+					diagonalCol = true;
 				}
 
-				if(player.speed!=0)
+				//UP RIGHT COLLISION
+				if(angle>=292 && angle<=337)
+				{
+					this.vspeed = (this.vspeed<0)?-this.vspeed:this.vspeed;
+					this.hspeed = (this.hspeed>0)?-this.hspeed:this.hspeed;
+					diagonalCol = true;
+				}
+
+				//DOWN RIGHT COLLISION
+				if(angle>=22 && angle<=67)
+				{
+					this.vspeed = (this.vspeed>0)?-this.vspeed:this.vspeed;
+					this.hspeed = (this.hspeed>0)?-this.hspeed:this.hspeed;
+					diagonalCol = true;
+				}
+
+				//DOWN LEFT COLLISION
+				if(angle>=112 && angle<=157)
+				{
+					this.vspeed = (this.vspeed>0)?-this.vspeed:this.vspeed;
+					this.hspeed = (this.hspeed<0)?-this.hspeed:this.hspeed;
+					diagonalCol = true;
+				}
+
+
+
+				if(player.speed!=0 && !diagonalCol)
 					this.vspeed = player.speed/2;
 				break;
 			}
