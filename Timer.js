a796037940f647e4e35f25cb4fad5a4a0df771b3
Franco <francolmenagar2@hotmail.com>
2018-02-19 09:55:14 -0400

    Now timer apparently works well

--- Timer.js
+++ Timer.js
@@ -3,8 +3,11 @@ class Timer
 	constructor()
 	{
 		this.start = null;
-		this.end = null;
 		this.fps = 1000/60;
+		this.lag = 0;
+
+		let date = new Date();
+		this.end = date.getTime();
 	}
 
 
@@ -21,20 +24,29 @@ class Timer
 
 	timerStart()
 	{
-		this.start = Date.now();
+		this.start = this.end;
 	}
 
 	timerEnd()
 	{
-		this.end = Date.now();
+		let date = new Date()
+		this.end =date.getTime();
 	}
 
 	render(update)
 	{
 		let difference = this.end-this.start;
-		console.log('Difference: '+difference);
+		// this.lag+=3;
 		if(difference < this.fps)
+		{		
+			console.log('Difference: '+difference+'. Delayed: '+(this.fps-difference)+'. TOTAL: '+(difference+(this.fps-difference)));
 			setTimeout(update,(this.fps-difference));
-			// setTimeout(function(){},(this.fps)-(this.end-this.start));
+		}
+		else
+		{
+			setTimeout(update,1);
+
+			// setTimeout(update,this.lag);
+		}
 	}
 }
