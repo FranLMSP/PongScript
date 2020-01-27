427b682dd888bfcf668785d45cbd254f51488f92
Franco Colmenarez <francoacg1@gmail.com>
2020-01-27 18:59:19 -0500

    Cleaning code in Input.js

--- Input.js
+++ Input.js
@@ -2,156 +2,153 @@ class Input
 {
 	constructor()
 	{
-		document.addEventListener('keydown',this.pressedKey.bind(this));
-		document.addEventListener('keyup',this.releasedKey.bind(this));
-		this.pressedKeys = [];
+		document.addEventListener('keydown', this.pressedKey.bind(this))
+		document.addEventListener('keyup', this.releasedKey.bind(this))
+		this.pressedKeys = []
 	}
 
 
 	pressedKey(event)
 	{
-		// this.action();38 40 37 39
-		switch(event.keyCode)
-		{
+		switch(event.keyCode) {
 			case 8: //backspace
-				this.pressedKeys.player2Back = true;
-				break;
+				this.pressedKeys.player2Back = true
+				break
 			case 74: //J
-				this.pressedKeys.player1Back = true;
-				break;
+				this.pressedKeys.player1Back = true
+				break
 			case 13: //ENTER
-				this.pressedKeys.player2Action = true;
-				break;
+				this.pressedKeys.player2Action = true
+				break
 			case 75: //K
-				this.pressedKeys.player1Action = true;
-				break;
+				this.pressedKeys.player1Action = true
+				break
 			case 38: //UP Arrow
-				this.pressedKeys.player2Up = true;
-				break;
+				this.pressedKeys.player2Up = true
+				break
 			case 87: //W
-				this.pressedKeys.player1Up = true;
-				break;
+				this.pressedKeys.player1Up = true
+				break
 			case 40: //Down Arrow
-				this.pressedKeys.player2Down = true;
-				break;
+				this.pressedKeys.player2Down = true
+				break
 			case 83: //S
-				this.pressedKeys.player1Down = true;
-				break;
+				this.pressedKeys.player1Down = true
+				break
 			case 37: //Left Arrow
-				this.pressedKeys.player2Left = true;
-				break;
+				this.pressedKeys.player2Left = true
+				break
 			case 65: //A
-				this.pressedKeys.player1Left = true;
-				break;
+				this.pressedKeys.player1Left = true
+				break
 			case 39: //Right Arrow
-				this.pressedKeys.player2Right = true;
-				break;
+				this.pressedKeys.player2Right = true
+				break
 			case 68: //D
-				this.pressedKeys.player1Right = true;
-				break;
+				this.pressedKeys.player1Right = true
+				break
 			default:
-				// console.log(event.keyCode);
-				break; 
+				// console.log(event.keyCode)
+				break
 		}
 	}
 
 	releasedKey(event)
 	{
-		switch(event.keyCode)
-		{
+		switch(event.keyCode) {
 			case 8: //backspace
-				this.pressedKeys.player2Back = false;
-				break;
+				this.pressedKeys.player2Back = false
+				break
 			case 74: //J
-				this.pressedKeys.player1Back = false;
-				break;
+				this.pressedKeys.player1Back = false
+				break
 			case 13: //ENTER
-				this.pressedKeys.player2Action = false;
-				break;
+				this.pressedKeys.player2Action = false
+				break
 			case 75: //K
-				this.pressedKeys.player1Action = false;
-				break;
+				this.pressedKeys.player1Action = false
+				break
 			case 38: //UP Arrow
-				this.pressedKeys.player2Up = false;
-				break;
+				this.pressedKeys.player2Up = false
+				break
 			case 87: //W
-				this.pressedKeys.player1Up = false;
-				break;
+				this.pressedKeys.player1Up = false
+				break
 			case 40: //Down Arrow
-				this.pressedKeys.player2Down = false;
-				break;
+				this.pressedKeys.player2Down = false
+				break
 			case 83: //S
-				this.pressedKeys.player1Down = false;
-				break;
+				this.pressedKeys.player1Down = false
+				break
 			case 37: //Left Arrow
-				this.pressedKeys.player2Left = false;
-				break;
+				this.pressedKeys.player2Left = false
+				break
 			case 65: //A
-				this.pressedKeys.player1Left = false;
-				break;
+				this.pressedKeys.player1Left = false
+				break
 			case 39: //Right Arrow
-				this.pressedKeys.player2Right = false;
-				break;
+				this.pressedKeys.player2Right = false
+				break
 			case 68: //D
-				this.pressedKeys.player1Right = false;
-				break;
+				this.pressedKeys.player1Right = false
+				break
 			default:
-				// console.log(event.keyCode);
-				break; 
+				// console.log(event.keyCode)
+				break
 		}
 
 	}
 
 	heldKey(key)
 	{
-		return this.pressedKeys[key];
+		return this.pressedKeys[key]
 	}
 
 	action()
 	{
-		alert('AAA!');
+		alert('AAA!')
 	}
 
 	keyUp(cb)
 	{
-		cb();
+		cb()
 	}
 
 	keyDown(cb)
 	{
-		cb();
+		cb()
 
 	}
 
 	keyLeft(cb)
 	{
-		cb();
+		cb()
 
 	}
 
 	keyRight(cb)
 	{
-		cb();
+		cb()
 
 	}
 
 	keyAction(cb)
 	{
-		cb();
+		cb()
 	}
 
 	keyBack(cb)
 	{
-		cb();
+		cb()
 	}
 
 	beginFrame()
 	{
-		// this.pressedKeys = [];
+		// this.pressedKeys = []
 	}
 
 	endFrame()
 	{
-		this.pressed = true;
+		this.pressed = true
 	}
 }
